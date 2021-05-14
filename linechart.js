16f86ed96ee4013247d6c9a033945dc983e83292
Cedric-json <82878018+Cedric-json@users.noreply.github.com>
2021-05-14 12:17:10 +0200

    margin erstellt

---
+++ linechart.js
@@ -0,0 +1,15 @@
+// Werte für das Fenster festlegen 
+var margin = {top: 30, right: 50, bottom: 30, left: 50 }    // Randabstände
+    , width = window = window.innerWidth - margin.left-margin.right     // rechnet die Breite des Fenster
+    , height = window = window.innerWidth - margin.top - margin.bottom; // rechnet die Höhe des Fenster 
+
+
+
+d3.json("data/data.json").then((data) => {
+
+
+   
+
+
+
+})
